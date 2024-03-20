685da83c6000d6b2b82738095abef9eae4d65325
edwin <edwinkun@gmail.com>
2024-03-20 13:19:06 +0700

    simulating a background thread :bat: :carrot:

--- src/main/java/com/edw/service/IndexService.java
+++ src/main/java/com/edw/service/IndexService.java
@@ -1,10 +1,14 @@
 package com.edw.service;
 
 import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * <pre>
@@ -19,11 +23,28 @@ public class IndexService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-    void onStop(@Observes ShutdownEvent ev) throws InterruptedException {
+    private Executor executorSrvice = Executors.newFixedThreadPool(1);
+
+    @Startup
+    public void init() {
+        executorSrvice.execute(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000l);
+                    logger.info("Inside Thread");
+                } catch (Exception ex) {
+                    logger.error(ex.getMessage(), ex);
+                }
+
+            }
+        });
+    }
+
+    public void onStop(@Observes ShutdownEvent ev) throws InterruptedException {
         logger.info("Application received SIGTERM signal");
 
         for (int i = 0; i < 5; i ++) {
-            Thread.sleep(1000l);
+            Thread.sleep(5000l);
             logger.info("waiting for {} ", i);
         }
 
